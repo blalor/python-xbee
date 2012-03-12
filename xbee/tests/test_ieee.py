8937f9144a001bc49f67082bb227678f58162eea
Brian Lalor <blalor@bravo5.org>
2012-03-12 06:16:07 -0400

    Test data was escaped :-(

--- xbee/tests/test_ieee.py
+++ xbee/tests/test_ieee.py
@@ -493,28 +493,29 @@ class TestReadFromDevice(unittest.TestCase):
         """
         Read and parse an RX packet from an XBee-Pro 900MHz module.
         """
-        data = \
-            '\x7e' + \
-            '\x00\x4c' + \
-            '\x90' + \
-            '\x00' + \
-            '}3\xa2\x00@x\xcd9' + \
-            '\xff\xfe' + \
-            '\x02' + \
-            '\x00\x0f\x0e/AccelX\x00\x00\x00\x00\x00\x00\xc0L?AccelY\x00\x00\x00\x00\x00\x00\xc0L?AccelZ\x00\x00\x00\x00\x00\x00\xc0L?Yaw\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0L' + \
-            'c'
+        data = '~\x00L\x90\x00}3\xa2\x00@x\xcd9\xff\xfe\x02\x00\x0f\x0e/AccelX\x00\x00\x00\x00\x00\x00\xc0L?AccelY\x00\x00\x00\x00\x00\x00\xc0L?AccelZ\x00\x00\x00\x00\x00\x00\xc0L?Yaw\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0L?\xc1'
 
         device = FakeReadDevice(data)
-        xbee = XBee(device)
+        xbee = XBee(device, escaped=True)
         
         info = xbee.wait_read_frame()
+
+        # import pprint
+        # pprint.pprint(info)
+        ## {'frame_id': '\x00',
+        ##  'id': 'rx_pro',
+        ##  'reserved': '\xfe\x02',
+        ##  'rf_data': '\x0f\x0e/AccelX\x00\x00\x00\x00\x00\x00\xc0L?AccelY\x00\x00\x00\x00\x00\x00\xc0L?AccelZ\x00\x00\x00\x00\x00\x00\xc0L?Yaw\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0L?',
+        ##  'rx_opt': '\x00',
+        ##  'source_addr_long': '\x13\xa2\x00@x\xcd9\xff'}
+
         expected_info = {
-            'id':'rx_pro',
-            'frame_id':'\x00',
-            'source_addr_long': '\x7d\x33\xa2\x00\x40\x78\xcd\x39',
-            'reserved': '\xff\xfe',
-            'rx_opt': '\x02',
-            'rf_data':'\x00\x0f\x0e/AccelX\x00\x00\x00\x00\x00\x00\xc0L?AccelY\x00\x00\x00\x00\x00\x00\xc0L?AccelZ\x00\x00\x00\x00\x00\x00\xc0L?Yaw\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0L'
+            'id'              :'rx_pro',
+            'frame_id'        :'\x00',
+            'source_addr_long':'\x13\xa2\x00\x40\x78\xcd\x39\xff',
+            'reserved'        :'\xfe\x02',
+            'rx_opt'          :'\x00',
+            'rf_data'         :'\x0f\x0e/AccelX\x00\x00\x00\x00\x00\x00\xc0L?AccelY\x00\x00\x00\x00\x00\x00\xc0L?AccelZ\x00\x00\x00\x00\x00\x00\xc0L?Yaw\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0L?'
         }
 
         self.maxDiff = None
